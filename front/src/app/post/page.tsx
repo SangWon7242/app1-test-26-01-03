15a56a0d1241d9d77e2a0967bcca171bd309c5ff
SangWon7242 <gotkdqja@gmail.com>
2026-01-03 15:46:14 +0900

    work

---
+++ front/src/app/post/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+export default function PostPage() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const postResp = await fetch("http://localhost:8080/api/v1/posts");
+      const posts = await postResp.json();
+      setPosts(posts);
+    };
+
+    fetchPosts();
+  }, []);
+
+  console.log(posts);
+
+  return (
+    <div>
+      <h1>글 목록</h1>
+      <ul>
+        {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
+        {posts.map((post: any) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
